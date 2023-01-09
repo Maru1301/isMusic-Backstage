3a72d73463740f0a16c6b8ab523fbfbb9c8e409e
MaruLin <how7633818@gmail.com>
2023-01-09 10:52:40 +0800

    Song Genre created
    # Conflicts:
    #	iSMusic/Views/Shared/_Layout.cshtml
    #	iSMusic/Views/Shared/_SongLayout.cshtml
    #	iSMusic/Views/_ViewStart.cshtml
    #	iSMusic/iSMusic.csproj

---
+++ iSMusic/Models/DTOs/SongGenreDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iSMusic.Models.DTOs
+{
+	public class SongGenreDTO
+	{
+		public int Id { get; set; }
+
+		public string GenreName { get; set; }
+	}
+}
---
+++ iSMusic/Models/Services/Interfaces/ISongGenreRepository.cs
@@ -0,0 +1,25 @@
+﻿using iSMusic.Models.DTOs;
+using iSMusic.Models.EFModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iSMusic.Models.Services.Interfaces
+{
+	public interface ISongGenreRepository
+	{
+		void Create(SongGenreDTO dto);
+
+		List<SongGenre> FindAll(int pageSize, int recordStartIndex, string genreName);
+
+		SongGenre FindById(int id);
+
+		int GetTotalRecordsNum();
+
+		void Edit(SongGenreDTO dto);
+
+		void Delete(SongGenreDTO dto);
+	}
+}
