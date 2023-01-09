e29c2b34e906a48f092ae4ceb4c54899271aafa8
MaruLin <how7633818@gmail.com>
2023-01-09 14:59:47 +0800

    Album created
    # Conflicts:
    #	iSMusic/iSMusic.csproj

---
+++ iSMusic/Models/DTOs/AlbumDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iSMusic.Models.DTOs
+{
+	public class AlbumDTO
+	{
+		public int id { get; set; }
+		public string albumName { get; set; }
+
+		public string albumCoverPath { get; set; }
+
+		public HttpPostedFileBase CoverFile { get; set; }
+
+		public DateTime released { get; set; }
+
+		public string description { get; set; }
+
+		public int mainArtistId { get; set; }
+
+		public List<int> songIdList { get; set; }
+	}
+}
