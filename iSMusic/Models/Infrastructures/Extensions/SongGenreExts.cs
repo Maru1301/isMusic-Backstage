acad74a9c3120b45288532bbb560c39f89e88ed2
MaruLin <how7633818@gmail.com>
2023-01-09 10:52:40 +0800

    Song Genre created

---
+++ iSMusic/Models/Infrastructures/Extensions/SongGenreExts.cs
@@ -0,0 +1,29 @@
+﻿using iSMusic.Models.DTOs;
+using iSMusic.Models.EFModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iSMusic.Models.Infrastructures.Extensions
+{
+	public static class SongGenreExts
+	{
+		public static SongGenreDTO ToDTO(this SongGenre source)
+		{
+			return new SongGenreDTO
+			{
+				Id = source.id,
+				GenreName = source.genreName,
+			};
+		}
+		public static SongGenre ToEntity(this SongGenreDTO source)
+		{
+			return new SongGenre
+			{
+				id = source.Id,
+				genreName = source.GenreName,
+			};
+		}
+	}
+}
