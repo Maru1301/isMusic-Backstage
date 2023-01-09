2a37556e6ce5ed7dcd4d26ff5dc423e09e6d024b
MaruLin <how7633818@gmail.com>
2023-01-09 14:59:47 +0800

    Album created

---
+++ iSMusic/Models/ViewModels/AlbumCreateVM.cs
@@ -0,0 +1,45 @@
+﻿using iSMusic.Models.EFModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace iSMusic.Models.ViewModels
+{
+	public class AlbumCreateVM
+	{
+		[Required]
+		[StringLength(50)]
+		[Display(Name = "專輯名稱*")]
+		public string albumName { get; set; }
+
+		public string albumCoverPath { get; set; }
+
+		[Required]
+		[Display(Name = "音樂封面檔案*")]
+		public HttpPostedFileBase CoverFile { get; set; }
+
+		[Column(TypeName = "date")]
+		[Display(Name = "發行時間*")]
+		public DateTime released { get; set; }
+
+		[Required]
+		[StringLength(3000)]
+		[Display(Name = "描述*")]
+		public string description { get; set; }
+
+		[Display(Name = "主要表演者*")]
+		public int mainArtistId { get; set; }
+
+		[Display(Name = "歌曲*")]
+		public List<int> songIdList { get; set; }
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+		public virtual ICollection<Album_Song_Metadata> Album_Song_Metadata { get; set; }
+
+		public virtual Artist Artist { get; set; }
+	}
+}
---
+++ iSMusic/Models/ViewModels/AlbumIndexVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iSMusic.Models.ViewModels
+{
+	public class AlbumIndexVM
+	{
+		public int id { get; set; }
+		public string albumName { get; set; }
+
+		public DateTime released { get; set; }
+
+		public string mainArtistName { get; set; }
+	}
+}
