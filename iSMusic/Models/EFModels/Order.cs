97f248309d9346eb9da87e995b2a1364359c5ee5
Hung08 <sun60703@gmail.com>
2023-01-10 19:59:27 +0800

    新增商品頁面

--- iSMusic/Models/EFModels/Order.cs
+++ iSMusic/Models/EFModels/Order.cs
@@ -45,6 +45,7 @@ namespace iSMusic.Models.EFModels
 
         public virtual Member Member { get; set; }
 
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Order_Product_Metadata> Order_Product_Metadata { get; set; }
     }
