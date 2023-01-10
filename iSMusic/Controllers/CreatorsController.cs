0bc4321e64dd1808429647861ed804edf8f7adce
MaruLin <how7633818@gmail.com>
2023-01-10 14:47:33 +0800

    Creators created

--- iSMusic/Controllers/CreatorsController.cs
+++ iSMusic/Controllers/CreatorsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using iSMusic.Models.EFModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,31 @@ namespace iSMusic.Controllers
 {
     public class CreatorsController : Controller
     {
+        private AppDbContext db;
+
+        public CreatorsController()
+        {
+            db = new AppDbContext();
+        }
+
         // GET: Creators
         public ActionResult Index()
         {
+            var data = db.Creators.ToList();
+
+            return View(data);
+        }
+
+        public ActionResult AddNewCreator()
+        {
             return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            var data = db.Creators.Find(id);
+
+            return View(data);
         }
     }
 }
--- iSMusic/Models/EFModels/AppDbContext.cs
+++ iSMusic/Models/EFModels/AppDbContext.cs
@@ -8,7 +8,7 @@ namespace iSMusic.Models.EFModels
 	public partial class AppDbContext : DbContext
 	{
 		public AppDbContext()
-			: base("name=AppDbContext")
+			: base("name=AppConnStr")
 		{
 		}
 
