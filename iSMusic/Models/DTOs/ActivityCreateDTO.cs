75126c0a1d927f92314bf8330a13a191c231a5bc
vintagehsieh <vintagehsieh@gmail.com>
2023-01-13 23:30:20 +0800

    補上Acitivities
    CURD已完成

---
+++ iSMusic/Models/DTOs/ActivityCreateDTO.cs
@@ -0,0 +1,53 @@
+﻿using isMusic.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace isMusic.Models.DTOs
+{
+    public class ActivityCreateDTO
+    {
+        public int id { get; set; }
+        public string activityName { get; set; }
+
+        public DateTime activityStartTime { get; set; }
+
+        public DateTime activityEndTime { get; set; }
+        public string activityLocation { get; set; }
+
+        public int activityTypeId { get; set; }
+
+        public string activityInfo { get; set; }
+
+        public int activityOrganizerId { get; set; }
+
+        public string activityImagePath { get; set; }
+
+        public bool publishedStatus { get; set; }
+
+        public int checkedById { get; set; }
+    }
+
+    public static class ActivityCreateDTOExts
+    {
+        public static ActivityCreateDTO ToDTO(this ActivityCreateVM source)
+        {
+
+            return new ActivityCreateDTO
+            {
+                id = source.id,
+                activityName = source.activityName,
+                activityStartTime = source.activityStartTime,
+                activityEndTime = source.activityEndTime,
+                activityLocation = source.activityLocation,
+                activityTypeId = source.activityTypeId,
+                activityInfo = source.activityInfo,
+                activityImagePath = source.activityImagePath,
+                activityOrganizerId = source.activityOrganizerId,
+                publishedStatus = source.publishedStatus,
+                checkedById = source.checkedById,
+            };
+        }
+    }
+}
